2d8611268d44682e54bd27fb99379c45b0e38dbf
jinnyw <jinnyw@uber.com>
2025-06-30 14:22:39 -0700

    [benchmark] flatten wildcard imports

--- benchmarks/src/main/java/org/opensearch/benchmark/ts/head/HeadAppendBenchmark.java
+++ benchmarks/src/main/java/org/opensearch/benchmark/ts/head/HeadAppendBenchmark.java
@@ -8,19 +8,30 @@
 
 package org.opensearch.benchmark.ts.head;
 
-import org.openjdk.jmh.annotations.*;
-import org.openjdk.jmh.infra.Blackhole;
 import org.opensearch.ts.head.Head;
 import org.opensearch.ts.head.HeadAppender;
 import org.opensearch.ts.model.Labels;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Fork(1)
 @Warmup(iterations = 1)
@@ -33,7 +44,7 @@ public class HeadAppendBenchmark {
     @Param({ "10000", "100000" })
     private int numSeries; // numbe rof unique series
 
-    @Param({ "10000"})
+    @Param({ "10000" })
     private int sampleBatchSize; // number of samples to append in one batch
 
     @Param({ "12" })
@@ -44,7 +55,6 @@ public class HeadAppendBenchmark {
 
     @Param({ "0.05", "0.5" })
     private double labelOverlapFactor; // fraction of total labels in the shared pool relative to total (smaller => more overlap)
-
 
     public Head head;
 
